ba43a53ffb863ce084ada52da2f321669903c43b
diegoxido <balladares.diego@gmail.com>
2017-03-12 18:48:41 -0300

    primer commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'wild-rice.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: /src/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['latest-minimal', 'react'],
+              plugins: ['transform-object-rest-spread'],
+            },
+          },
+          'eslint-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        include: /src/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+    ],
+  },
+};
